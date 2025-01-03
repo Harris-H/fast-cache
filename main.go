e35c1e515289840c0d095043f146ca495a5b117b
Herio <haohe@buaa.edu.cn>
2025-01-04 00:13:44 +0800

    Add use sample.

--- main.go
+++ main.go
@@ -6,17 +6,27 @@ import (
 )
 
 func main() {
-	l, _ := lru.New[int, any](5)
-	l.AddMany([]int{1, 2, 3, 4, 5}, []any{1, 2, 3, 4, 5})
-	fmt.Println(l.GetOldest())
-	l.Get(1)
-	fmt.Println(l.GetOldest())
-	l.Add(6, 6*5)
-	fmt.Println(l.GetOldest())
-	_, err := l.Resize(-1)
-	if err != nil {
-		fmt.Println(err)
-		return
+	l, _ := lru.New[int, string](5)
+	l.AddMany([]int{1, 2, 3, 4, 5}, []string{"Java", "Go", "Python", "C++", "C"})
+	keys := l.Keys(false)
+	fmt.Println("keys: ", keys)
+	keysOrderedByNew := l.Keys(true)
+	fmt.Println("keysOrderedByNew 1: ", keysOrderedByNew)
+	value, ok := l.Get(3)
+	if ok {
+		fmt.Println("key: ", 3, " value: ", value)
 	}
-	l.Add(1, 1)
+	fmt.Println("keysOrderedByNew 2: ", keysOrderedByNew)
+	fmt.Println("Add (6,Rust): ")
+	l.Add(6, "Rust")
+	keysOrderedByNew = l.Keys(true)
+	fmt.Println("keysOrderedByNew 3: ", keysOrderedByNew)
+	/*
+		keys:  [1 2 3 4 5]
+		keysOrderedByNew 1:  [5 4 3 2 1]
+		key:  3  value:  Python
+		keysOrderedByNew 2:  [5 4 3 2 1]
+		Add (6,Rust):
+		keysOrderedByNew 3:  [6 3 5 4 2]
+	*/
 }
